74d5b815ade48903f1d77b307575de61c09a8a21
miqela97 <132955511+miqela97@users.noreply.github.com>
2023-07-12 03:00:56 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {
+    // we'll fill this in shortly
+  }
+  function subtract() {
+    // we'll fill this in shortly
+  }
+  function divide() {
+    // we'll fill this in shortly
+  }
+  function multiply() {
+    // we'll fill this in shortly
+  }
+  function add(a, b) {
+    return a+b;
+  }
+  function subtract(a, b) {
+    return a-b;
+  }
+  function multiply(a, b) {
+    return a*b;
+  }
+  function divide(a, b) {
+    return a/b;
+  }
+  function increment(){
+    number = 5;
+    number--;
+    return number;
+  }
+  function decrement(){
+    let n = 6;
+    n--;
+    return n;
+  }
+
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
